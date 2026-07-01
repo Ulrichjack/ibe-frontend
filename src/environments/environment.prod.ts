ff4dfaeecb835fe42a41a9336fd9460a4c37d816
Ulrichjack <jackeboyeur244@gmail.com>
2026-07-01 08:15:38 +0100

    fix:update prod env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://institut-app-backend.onrender.com/api',
+  actuatorUrl: 'https://institut-app-backend.onrender.com/health',
+  whatsappNumber: '237690566836', // Remplace par le vrai numéro IBE plus tard
+  whatsappMessage: "Bonjour IBE ! J'aimerais des informations sur vos formations 😊",
+  cloudinaryCloudName: 'dsanuxbtn', // Remplace par ton vrai Cloud Name
+  cloudinaryUploadPreset: 'institue' 
+};
