ec5bfe5999603b63672321c16ce6adff4e9a1aed
Ulrichjack <jackeboyeur244@gmail.com>
2026-07-01 09:32:21 +0100

    fix:update prod env nmber whatsapp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://institut-app-backend.onrender.com/api',
   actuatorUrl: 'https://institut-app-backend.onrender.com/health',
-  whatsappNumber: '237690566836', // Remplace par le vrai numéro IBE plus tard
+  whatsappNumber: '237679358484', // Remplace par le vrai numéro IBE plus tard
   whatsappMessage: "Bonjour IBE ! J'aimerais des informations sur vos formations 😊",
   cloudinaryCloudName: 'dsanuxbtn', // Remplace par ton vrai Cloud Name
   cloudinaryUploadPreset: 'institue' 
